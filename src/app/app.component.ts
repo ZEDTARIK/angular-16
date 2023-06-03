dbfc42ee1a9a91e394a14d8fdd7fc4c9929a26e6
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-06-03 19:53:42 +0100

    use RXJS Interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit   {
+
+  seconds: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const compteur = interval(1000);
+    compteur.subscribe( {
+      next: (value) => this.seconds = value,
+      error: (err) => console.error(err),
+      complete:() => console.info("complete")
+    })
+  }
+
+
 }
